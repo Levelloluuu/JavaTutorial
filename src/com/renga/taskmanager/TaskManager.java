b42df689fa781910600a468abb780e29106cea1d
Levelloluuu <level12067474@gmail.com>
2025-08-10 21:21:01 +0900

    Taskクラスの作成（1件分の型）

--- src/com/renga/taskmanager/TaskManager.java
+++ src/com/renga/taskmanager/TaskManager.java
@@ -1,5 +1,24 @@
 package com.renga.taskmanager;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaskManager {
+        private final List<Task> tasks = new ArrayList<>();
+        private int nextId = 1;
+
+        public Task add(String title) {
+            if (title == null || title.trim().isEmpty()) {
+                System.out.println("タイトルは必須です");
+            }
+            Task task = new Task(nextId, title); //タスク追加
+            tasks.add(task);
+            nextId++;
+            return task;
+        }
+        public List<Task> list() {
+            return tasks;
+        }
 
 }
+

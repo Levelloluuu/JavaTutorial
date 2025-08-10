2ed0c9d631ce3fa9b8e8298f2b6e351688bcbdff
Levelloluuu <level12067474@gmail.com>
2025-08-10 22:11:03 +0900

    タスク追加・一覧表示機能を実装（TaskManager & Main）

--- src/com/renga/taskmanager/Main.java
+++ src/com/renga/taskmanager/Main.java
@@ -1,4 +1,38 @@
 package com.renga.taskmanager;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        TaskManager tm = new TaskManager();
+        Scanner sc = new Scanner(System.in);
+
+        while (true){
+            System.out.println("\n=== Task Manager ===");
+            System.out.println("1: 追加 2: リスト 0) 終了");
+            System.out.println(">");
+            String choice = sc.nextLine().trim();
+
+            switch (choice){
+                case "1":
+                    System.out.println("タイトル: ");
+                    String title = sc.nextLine();
+                    System.out.println(tm.add(title));
+                    break;
+
+                case "2":
+                    for (int i = 0; i < tm.list().size(); i++){
+                        System.out.println(tm.list().get(i));
+                    }
+                    break;
+                case "0":
+                    System.out.println("終了");
+                    break;
+
+                default:
+                    System.out.println("0～2を選んでね");
+
+            }
+        }
+    }
 }
